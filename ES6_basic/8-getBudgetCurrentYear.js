f39e50486983720684525f1fe9bdeae054436724
Zetrayo <josefeliciano.com98@gmail.com>
2024-11-13 12:24:29 +0100

    FIX : fixed indentation

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,15 +1,13 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {};
-  
-    // Using ES6 computed property names
-    budget[`income-${getCurrentYear()}`] = income;
-    budget[`gdp-${getCurrentYear()}`] = gdp;
-    budget[`capita-${getCurrentYear()}`] = capita;
-  
-    return budget;
-  }
+	const date = new Date();
+	return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+	const budget = {
+		[`income-${getCurrentYear()}`]: income,
+		[`gdp-${getCurrentYear()}`]: gdp,
+		[`capita-${getCurrentYear()}`]: capita,
+	};
+	return budget;
+}
