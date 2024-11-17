449cdab61a1f4fd7af3d9725770392787ef2b761
Zetrayo <josefeliciano.com98@gmail.com>
2024-11-17 18:45:22 +0100

    FIX : fixed things, maybe

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,21 +3,18 @@ import Building from './5-building.js';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('floors must be a number');
-    }
-    this._floors = value;
-  }
-
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
